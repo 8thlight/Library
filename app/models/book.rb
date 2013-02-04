54eb4cd9d9d24fb9ce5e3764117af3c1d6311df7
Takayuki Goto <tak.yuki@gmail.com>
2013-02-04 15:07:03 -0600

    book class testing

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
   attr_accessible :author, :isbn, :quantity, :title
+  validates :title, :uniqueness => true
+  validates :isbn, :uniqueness => true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe Book do
-  #pending "add some examples to (or delete) #{__FILE__}"
-  rspec_book = Book.new(title: "rspec book", author: "Taka Goto", isbn: "1234abcd", quantity: 2)
+  before(:each) do
+    @attributes =
+      { :title    => "Rails Tutorial 3",
+        :author   => "Michael Hartl",
+        :isbn     => "1234abcd",
+        :quantity => 2
+    }
+  end
+  it "should reject duplicate titles" do
+    Book.create!(title: "Rails Tutorial",author: "Michael Hartl",isbn: "124432",quantity: 1)
+    book_with_same_name = Book.new(title: "Rails Tutorial",author: "Anish Kothari",isbn: "12333321",quantity: 31)
+    book_with_same_name.should_not be_valid
+  end
+
+  it "should reject duplicate ISBNs" do
+    Book.create!(title: "Rails Tutorial",author: "Michael Hartl",isbn: "124432",quantity: 1)
+    book_with_same_isbn = Book.new(title: "Rails ",author: "Anish Kothari",isbn: "124432",quantity: 31)
+    book_with_same_isbn.should_not be_valid
+  end
 end
