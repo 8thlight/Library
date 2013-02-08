fd0ba99f8e29d16914ec952f5b9fae6ae0e53966
Anish Kothari <anishkothari@gmail.com>
2013-02-08 15:55:03 -0600

    fixed index view test

--- spec/views/books/index.html.erb_spec.rb
+++ spec/views/books/index.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "books/index.html.erb" do
   it "displays the text 'List of Books'" do
+    @books = []
     render
     rendered.should contain("List of Books")
   end
