dc1da30c583ea6483fc4a67b60cdfca32aee8cff
Takayuki Goto <tak.yuki@gmail.com>
2013-02-13 15:08:43 -0600

    heroku api:

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,15 +8,16 @@ class Book < ActiveRecord::Base
   validates :isbn, :uniqueness => true
   validates :quantity, :presence => true, :numericality => { :greater_than_or_equal_to => 0 }
 
+  user_ip = request.remote_ip
   def get_title
-     GoogleBooks.search("isbn:#{isbn}").first.title
+     GoogleBooks.search("isbn:#{isbn}", user_ip).first.title
   end
 
   def get_author
-      GoogleBooks.search("isbn:#{isbn}").first.authors
+      GoogleBooks.search("isbn:#{isbn}", user_ip).first.authors
   end
 
   def get_image
-    GoogleBooks.search("isbn:#{isbn}").first.image_link
+    GoogleBooks.search("isbn:#{isbn}", user_ip).first.image_link
   end
 end
