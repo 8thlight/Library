a166550bb8c51533658ee1ad1f5f56c50bc10461
Takayuki Goto <tak.yuki@gmail.com>
2013-03-11 11:57:14 -0500

    removed Resque

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,4 @@ $redis = Redis.new(:host => 'localhost', :port => 6379)
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-Resque.redis = REDIS
 
