7cf1f5af8a5a858fac246bf28a09e9f38f6fb592
Takayuki Goto <tak.yuki@gmail.com>
2013-03-11 11:50:56 -0500

    fixing heroku with redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,5 @@ $redis = Redis.new(:host => 'localhost', :port => 6379)
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
 
