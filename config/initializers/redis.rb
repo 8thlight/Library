355cca9e786cd36d1f95062e0c4ebbb27355b8c2
Takayuki Goto <tak.yuki@gmail.com>
2013-03-11 12:56:49 -0500

    heroku trial #200

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,8 @@
 $redis = Redis.new(:host => 'localhost', :port => 6379)
 
 #uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
-#REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 redis_url = ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379/0/myapp"
 Library::Application.config.cache_store = :redis_store, redis_url
 Library::Application.config.session_store :redis_store, redis_server: redis_url
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
