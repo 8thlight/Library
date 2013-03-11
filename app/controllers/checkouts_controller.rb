dc864385150adf1a58df8e006273cfe9a4904f9e
Takayuki Goto <tak.yuki@gmail.com>
2013-03-11 10:12:57 -0500

    deleted random file

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -6,14 +6,19 @@ class CheckoutsController < ApplicationController
     @check_out = Checkout.new(book_id: @book_id, user_id: session[:user_id], check_out_date: Time.now)
     @user_name =  User.find(session[:user_id]).name
 
+    mutex = Mutex.new
+
     if unique?(@check_out) && !@book.quantity_left.zero? && check_waitlist(@book_id, @user_name)
       @check_out.save
-      decrement_quantity(@book)
+      mutex.synchronize do
+        decrement_quantity(@book)
+      end
       flash[:notice] = "the checkout was successful"
     else
       flash[:notice] = "Sorry, book is unavailable. You may have already checkout this book."
     end
     redirect_to :action => "index"
+
   end
 
   def decrement_quantity(book)
