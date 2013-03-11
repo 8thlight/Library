2bacff17d91d9fca8eb324a4599d03ee57517ab1
Takayuki Goto <tak.yuki@gmail.com>
2013-03-11 11:39:51 -0500

    heroku with travis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
 $redis = Redis.new(:host => 'localhost', :port => 6379)
 
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
