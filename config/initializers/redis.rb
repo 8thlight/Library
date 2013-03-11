7c18ce9581505121cc7f2cc5e4fe346e02632fd5
Takayuki Goto <tak.yuki@gmail.com>
2013-03-11 12:16:59 -0500

    still on heroku and redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,5 @@
 $redis = Redis.new(:host => 'localhost', :port => 6379)
 
-#uri = URI.parse(ENV["REDISTOGO_URL"])
-
-uri = Redis.connect(:url => ENV['REDISTOGO_URL'])
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
