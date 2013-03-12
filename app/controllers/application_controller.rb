18e67922b9423ee308c3e40b3f709b962f26a58f
Takayuki Goto <tak.yuki@gmail.com>
2013-03-12 08:49:58 -0500

    moved methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,53 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :user_signed_in?, :correct_user?
 
+  def user_new_in_list?(user, book)
+    Waitinglist.where(user_id: session[:user_id], book_id: book).empty? &&
+    Checkout.where(user_id: session[:user_id], book_id: book).empty?
+  end
+
+  def increment_quantity(book)
+    book.quantity_left += 1
+    book.update_attributes(params[:book])
+  end
+
+  def decrement_quantity(book)
+    book.quantity_left -= 1
+    book.update_attributes(params[:book])
+  end
+
+  def unique?(checkout)
+    unique = true
+    Checkout.all.each do |checkouts|
+      unique = checkout.user_id == checkouts.user_id && checkout.book_id == checkouts.book_id ? false : true
+    end
+    unique
+  end
+
+  def remove_from_waitinglist(book_id)
+    remove_from_waiting_list = Waitinglist.where(book_id: book_id).first
+    Waitinglist.destroy(remove_from_waiting_list)
+  end
+
+  def check_waitlist(book_id, user_name)
+    if Waitinglist.where(book_id: book_id).empty?
+      return true
+    else
+      if check_first_waitlist(book_id, user_name)
+        remove_from_waitinglist(book_id)
+        return true
+      else
+        return false
+      end
+    end
+  end
+
+  def check_first_waitlist(book_id, user_id)
+    @waiting_list = Waitinglist.where(book_id: book_id).first
+    return true if @waiting_list != nil && @waiting_list.user.id == user_id
+    false
+  end
+
   private
 
     def current_user
