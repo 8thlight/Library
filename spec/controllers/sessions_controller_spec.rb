8471aee49182757c93b05d0eb297477fa8714e94
Takayuki Goto <tak.yuki@gmail.com>
2013-03-12 18:09:24 -0500

    sessions controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe SessionsController do
+  describe "POST create" do
+    it "should redirect to home page" do
+    end
+  end
 
+  describe "#destroy" do
+    it "redirects to root after signing out" do
+      get 'destroy'
+      response.should redirect_to root_url
+    end
+  end
+
+  describe "GET new" do
+    it "directs to the authentication verification" do
+      get 'new'
+      response.should redirect_to '/auth/google_oauth2'
+    end
+  end
+
+  describe "#open_id" do
+    it "redirects to admin" do
+      get 'open_id'
+      response.should redirect_to '/auth/admin'
+    end
+  end
+
+  describe "#failure" do
+    it "redirects to to root url" do
+      get 'failure'
+      stub(:humanize)
+      response.should redirect_to root_url
+    end
+  end
 end
