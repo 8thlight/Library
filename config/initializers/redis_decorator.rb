b1e21b6f3241edca555c7b7e7a87eee97286330a
Takayuki Goto <tak.yuki@gmail.com>
2013-03-22 09:07:07 -0500

    redis and travis fixed

--- config/initializers/redis_decorator.rb
+++ config/initializers/redis_decorator.rb
@@ -4,22 +4,22 @@ module ApplicationDecorator
     CACHE_EXPIRE = 1.months
 
     def self.get_book_attr(isbn, attr)
-      add_book_to_redis(isbn) if REDIS.hlen("isbn_#{isbn}").zero?
+      add_book_to_redis(isbn, api_book(isbn)) if REDIS.hlen("isbn_#{isbn}").zero?
       REDIS.hget("isbn_#{isbn}", attr)
     end
 
     def self.get_google_book(isbn)
-      add_book_to_redis(isbn) if REDIS.hlen("isbn_#{isbn}").zero?
+      add_book_to_redis(isbn, api_book(isbn)) if REDIS.hlen("isbn_#{isbn}").zero?
       REDIS.hgetall("isbn_#{isbn}")
     end
 
-    def self.book(isbn)
-      @book ||= GoogleBooks.search("isbn:#{isbn}", :api_key => API_KEY).first
+    def self.api_book(isbn)
+      GoogleBooks.search("isbn:#{isbn}", :api_key => API_KEY).first
     end
 
     private
 
-    def self.add_book_to_redis(key)
+    def self.add_book_to_redis(key, book)
       if !book.nil?
         REDIS.hmset("isbn_#{key}", "title", book.title, "author", book.authors, "image", book.image_link)
         REDIS.expire(key,  CACHE_EXPIRE)
